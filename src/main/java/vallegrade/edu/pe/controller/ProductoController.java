09a8798144bfa1a6e4295e927a4e24e51133b0a7
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2025-10-01 09:08:38 -0500

    Update ProductoController.java

--- src/main/java/vallegrade/edu/pe/controller/ProductoController.java
+++ src/main/java/vallegrade/edu/pe/controller/ProductoController.java
@@ -1,6 +1,7 @@
 package vallegrade.edu.pe.controller;
 
 
+
 import vallegrade.edu.pe.model.Producto;
 import vallegrade.edu.pe.service.ProductoService;
 import vallegrade.edu.pe.view.FrmProducto;
@@ -8,16 +9,18 @@ import vallegrade.edu.pe.view.FrmProducto;
 import java.util.List;
 
 public class ProductoController {
-    private productoService service;
+    private ProductoService service;
     private FrmProducto vista;
 
-    public ProductoController(FrmProducto vista){
+    public ProductoController(FrmProducto vista) {
         this.vista = vista;
         this.service = new ProductoService();
         cargarProductos();
     }
-    private void cargarProductos(){
+
+    private void cargarProductos() {
         List<Producto> lista = service.obtenerProductos();
         vista.mostrarProductos(lista);
     }
 }
+
