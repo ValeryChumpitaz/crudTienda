c9c494562c4da170a0b70fb772fea8ee84320e49
Valery Chumpitaz <valery,chumpitaz@vallegrande.edu.pe>
2025-10-01 08:34:31 -0500

    v1

--- src/main/java/vallegrade/edu/pe/controller/ProductoController.java
+++ src/main/java/vallegrade/edu/pe/controller/ProductoController.java
@@ -8,5 +8,16 @@ import vallegrade.edu.pe.view.FrmProducto;
 import java.util.List;
 
 public class ProductoController {
+    private productoService service;
+    private FrmProducto vista;
 
+    public ProductoController(FrmProducto vista){
+        this.vista = vista;
+        this.service = new ProductoService();
+        cargarProductos();
+    }
+    private void cargarProductos(){
+        List<Producto> lista = service.obtenerProductos();
+        vista.mostrarProductos(lista);
+    }
 }
--- src/main/java/vallegrade/edu/pe/database/ConexionBD.java
+++ src/main/java/vallegrade/edu/pe/database/ConexionBD.java
@@ -9,4 +9,18 @@ import java.sql.DriverManager;
 
 public class ConexionBD {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/bd_tienda";
+    private static final String USER =  "root";
+    private static final String PASS = "Valery2025";
+
+    public static Connection getConnection(){
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL,USER,PASS);
+            System.out.println("Conexion exitosa a la BD");
+        } catch (Exception e){
+            System.out.println("Error en la conexion: " + e.getMessage());
+    }
+        return conn;
+}
 }
